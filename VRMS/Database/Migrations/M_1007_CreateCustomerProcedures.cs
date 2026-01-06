291cd7f56251c9e113c35db78de82ce1d7a40d88
chalen-dev <c.lusaya.142396.tc@umindanao.edu.ph>
2026-01-06 19:12:50 +0800

    feat(database): implement customer management procedures
    - Added CRUD stored procedures for customers table:
      - sp_customers_create
      - sp_customers_get_by_id
      - sp_customers_get_all
      - sp_customers_update
      - sp_customers_delete
    - Created migration M_1007 to manage customer procedures
    - Added support for customer types (Regular/VIP)
    - Included foreign key relationship with drivers_licenses
    - Organized procedures under Customers.Customer namespace
    - Standardized procedure structure with static Sql() methods

---
+++ VRMS/Database/Migrations/M_1007_CreateCustomerProcedures.cs
@@ -0,0 +1,28 @@
+﻿using VRMS.Database.StoredProcedures.Customers.Customer;
+
+namespace VRMS.Database.Migrations;
+
+public static class M_1007_CreateCustomerProcedures
+{
+    public static string Create() => $"""
+                                      DROP PROCEDURE IF EXISTS sp_customers_create;
+                                      DROP PROCEDURE IF EXISTS sp_customers_get_by_id;
+                                      DROP PROCEDURE IF EXISTS sp_customers_get_all;
+                                      DROP PROCEDURE IF EXISTS sp_customers_update;
+                                      DROP PROCEDURE IF EXISTS sp_customers_delete;
+
+                                      {SP_Customers_Create.Sql()}
+                                      {SP_Customers_GetById.Sql()}
+                                      {SP_Customers_GetAll.Sql()}
+                                      {SP_Customers_Update.Sql()}
+                                      {SP_Customers_Delete.Sql()}
+                                      """;
+
+    public static string Drop() => """
+                                   DROP PROCEDURE IF EXISTS sp_customers_create;
+                                   DROP PROCEDURE IF EXISTS sp_customers_get_by_id;
+                                   DROP PROCEDURE IF EXISTS sp_customers_get_all;
+                                   DROP PROCEDURE IF EXISTS sp_customers_update;
+                                   DROP PROCEDURE IF EXISTS sp_customers_delete;
+                                   """;
+}
---
+++ VRMS/Database/StoredProcedures/Customers/Customer/SP_Customers_Delete.cs
@@ -0,0 +1,14 @@
+﻿namespace VRMS.Database.StoredProcedures.Customers.Customer;
+
+public static class SP_Customers_Delete
+{
+    public static string Sql() => """
+                                  CREATE PROCEDURE sp_customers_delete (
+                                      IN p_customer_id INT
+                                  )
+                                  BEGIN
+                                      DELETE FROM customers
+                                      WHERE id = p_customer_id;
+                                  END;
+                                  """;
+}
